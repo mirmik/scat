edf96e7f0b0a90cc58b03f5f8b33947fa5f681ac
nfsorokin <sorokin@smitek-robotics.com>
2025-11-28 14:34:28 +0300

    Enable UTF-8 console support on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include "scat.h"
+#ifdef _WIN32
+#    include <windows.h>
+#endif
 
 int main(int argc, char **argv)
 {
+#ifdef _WIN32
+    // Переключаем кодовые страницы консоли на UTF-8, чтобы UTF-8 текст
+    // (включая кириллицу) нормально печатался в cmd/PowerShell.
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
     return scat_main(argc, argv);
 }
